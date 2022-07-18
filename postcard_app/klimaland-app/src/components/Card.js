77a095930cc38939c615649ac77793e12ccb47b9
johannahaha <johanna.hartmann@filmuniversitaet.de>
2022-07-18 16:18:10 +0200

    added key prop to class

--- postcard_app/klimaland-app/src/components/Card.js
+++ postcard_app/klimaland-app/src/components/Card.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Side from "./Side";
 
-const Card = ({ classProp, lk, section }) => {
+const Card = ({ classProp, lk, section, key}) => {
   return (
     <div className={classProp}>
       <Side lk={lk} section={section} />
