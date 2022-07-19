03d71789c245ba8198a805feb3580bd68e1dc607
johannahaha <johanna.hartmann@filmuniversitaet.de>
2022-07-19 15:35:23 +0200

    switch between postcard view (stack) and ordered view by clicking on card

--- postcard_app/klimaland-app/src/components/CardCollection.js
+++ postcard_app/klimaland-app/src/components/CardCollection.js
@@ -3,39 +3,36 @@ import { Component } from 'react'
 //components
 import Card from "./Card";
 
-//images
-import flip from "../img/buttons/flip.png";
-import switchCard from "../img/buttons/switch.png";
-//import shuffle from "../img/buttons/shuffle.png";
-import close from "../img/buttons/close.png";
 
 export default class CardCollection extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state = {activeCard: 0,cards:[]};
+        this.state = { cards: [] };
+        this.handleClickOnCard = this.handleClickOnCard.bind(this)
     }
 
-    handleClickOnCard(){
-        console.log("handle click now")
+    handleClickOnCard() {
         this.props.switchToPostcardView();
     }
 
     //generate card objects dynamically depending on mode
-    generateCards(){
+    generateCards() {
         let list;
         let classProp;
 
         console.log("generate cards")
 
-        if (this.props.postcardView){
-            list = this.props.cardSelection.map((element,i) => {                
-                const indexLeft = this.mod(this.state.activeCard - 1, this.props.cardSelection.length);
-                const indexRight =this.mod(this.state.activeCard + 1, this.props.cardSelection.length);
-    
+        if (this.props.postcardView) {
+            list = this.props.cardSelection.map((element, i) => {
+                const indexLeft = this.mod(this.props.activeCard - 1, this.props.cardSelection.length);
+                const indexRight = this.mod(this.props.activeCard + 1, this.props.cardSelection.length);
+
                 let classProp = "";
-    
-                if (i === this.state.activeCard) {
+
+                console.log(this.props.activeCard)
+
+                if (i === this.props.activeCard) {
                     classProp = "card card-active";
                 } else if (i === indexRight) {
                     classProp = "card card-right";
@@ -45,50 +42,49 @@ export default class CardCollection extends Component {
                     classProp = "card card-back";
                 }
 
-                return <Card lk={element.lk} section={element.section} key={i} classProp={classProp}/>
+                console.log(classProp)
+
+                return <Card lk={element.lk} section={element.section} key={i} classProp={classProp} isThumbnail={false} />
             });
 
-            console.log("postcardview cards generated")
+            console.log("postcardview cards generated", list)
+        }
+        else {
+            if (this.props.mode === "comparison") {
+                classProp = "card card-ordered"
+            }
+
+            else if (this.props.mode === "lk") {
+                classProp = "card card-ordered"
+            }
+
+            else if (this.props.mode === "shuffle") {
+                //todo: editors pick / shuffle mode
+                classProp = "card card-ordered"
+            }
+
+
+            list = this.props.cardSelection.map((element, i) =>
+                <Card lk={element.lk} section={element.section} key={i} classProp={classProp} clickOnCard={this.handleClickOnCard} isThumbnail={true} />
+            );
         }
 
-        else if (this.props.mode === "comparison") {
-            classProp = "card card-ordered"
-        }
-
-        else if (this.props.mode === "lk") {
-            classProp = "card card-ordered"
-        }
-
-        else if (this.props.mode === "shuffle"){
-            //todo: editors pick / shuffle mode
-            classProp = "card card-ordered"
-        }
-
-        list = this.props.cardSelection.map((element,i) =>
-            <Card lk={element.lk} section={element.section} key={i} classProp={classProp} clickOnCard={this.handleClickOnCard}/>
-        );
-
-        this.setState({cards:list})
+        this.setState({ cards: list })
         console.log(list)
     }
 
-    //const [activeCard, setActiveCard] = useState(0);
-
     //modulo helper function
-    mod(n,m){
+    mod(n, m) {
         let result = n % m;
         return result >= 0 ? result : result + m;
     };
 
-    //switch to next card in postcard view
-    nextCard() {
-        let newActiveCard = (this.state.activeCard + 1) % this.state.cards.length
-        this.setState({activeCard: newActiveCard});
-        //setActiveCard((activeCard + 1) % cards().length);
-    };
 
-    componentDidUpdate(prevProps){
-        if (this.props.cardSelection !== prevProps.cardSelection) {
+    componentDidUpdate(prevProps) {
+        if (this.props.cardSelection !== prevProps.cardSelection ||
+            this.props.postcardView !== prevProps.postcardView ||
+            this.props.activeCard !== prevProps.activeCard) {
+            console.log("props did update")
             this.generateCards();
         }
     }
@@ -96,47 +92,25 @@ export default class CardCollection extends Component {
     render() {
         return (
             <div className="card-collection">
-            
-            {/* <div className="card-container" >
+
+                {/* <div className="card-container" >
                 {cards()}
             </div> */}
-            {this.props.mode === "comparison" && !this.props.postcardView && <div className="card-container">  {this.state.cards} </div>}
-            {this.props.mode === "lk" && !this.props.postcardView && <div className="card-container">  {this.state.cards} </div>}
-            {/* {mode() === "shuffle" &&!postcardView && cards()} */}
-            {this.props.postcardView && <div className="card-container">
-                <div className="carousel">
-                    {this.state.cards}
-                    <Card
+                {this.props.mode === "comparison" && !this.props.postcardView && <div className="card-container">  {this.state.cards} </div>}
+                {this.props.mode === "lk" && !this.props.postcardView && <div className="card-container">  {this.state.cards} </div>}
+                {/* {mode() === "shuffle" &&!postcardView && cards()} */}
+                {this.props.postcardView && <div className="card-container">
+                    <div className="carousel">
+                        {this.state.cards}
+                        {/* <Card
                         key="9999"
                         classProp={"card"}
                         lk={"item.label"}
                         section="energy"
-                    />
-                </div>
-            </div>}
-
-            <div className="button-container">
-                <button className="close-button">
-                    <img
-                        src={close}
-                        className="close-button-img"
-                        alt="close-button-img"
-                    />
-                </button>
-
-                <button className="switch-button" onClick={this.nextCard}>
-                    <img
-                        src={switchCard}
-                        className="switch-button-img"
-                        alt="switch-button-img"
-                    />
-                </button>
-
-                <button className="flip-button">
-                    <img src={flip} className="flip-button-img" alt="flip-button-img" />
-                </button>
+                    /> */}
+                    </div>
+                </div>}
             </div>
-        </div>
         )
     }
 }
--- postcard_app/klimaland-app/src/components/Controls.js
+++ postcard_app/klimaland-app/src/components/Controls.js
@@ -4,209 +4,263 @@ import Select from "react-select";
 //our components
 import CardCollection from "./CardCollection";
 
+//images
+import flip from "../img/buttons/flip.png";
+import switchCard from "../img/buttons/switch.png";
+//import shuffle from "../img/buttons/shuffle.png";
+import close from "../img/buttons/close.png";
+
 export default class Controls extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      shuffleSelection: [
-        { lk: { value: "11" }, section: "mobility" },
-        { lk: "1011", section: "waste" },
-        { lk: "11", section: "mobility" },
-      ],
-      sections: [
-        {
-          label: "Mobilität",
-          value: "mobility",
-        },
-        {
-          label: "Gebäude",
-          value: "buildings",
-        },
-        {
-          label: "Energie",
-          value: "energy",
-        },
-        {
-          label: "Landwirtschaft",
-          value: "agriculture",
-        },
-        {
-          label: "Abfallentsorgung",
-          value: "waste",
-        },
-      ],
-      landkreise: [
-        {
-          label: "Flensburg",
-          value: "1011",
-        },
-        {
-          label: "Hamburg",
-          value: "2",
-        },
-        {
-          label: "Berlin",
-          value: "11",
-        },
-        {
-          label: "Deutschland",
-          value: "0",
-        },
-      ],
-      section: [],
-      landkreisSelection: [],
-      postcardView: false,
-      mode: "",
-      cardSelection: [],
-    };
-  }
+        this.landkreise = [
+            {
+                label: "Flensburg",
+                value: "1011",
+            },
+            {
+                label: "Hamburg",
+                value: "2",
+            },
+            {
+                label: "Berlin",
+                value: "11",
+            },
+            {
+                label: "Deutschland",
+                value: "0",
+            },
+        ]
 
-  setStateAsync(state) {
-    return new Promise((resolve) => {
-      this.setState(state, resolve);
-    });
-  }
+        this.sections = [
+            {
+                label: "Mobilität",
+                value: "mobility",
+            },
+            {
+                label: "Gebäude",
+                value: "buildings",
+            },
+            {
+                label: "Energie",
+                value: "energy",
+            },
+            {
+                label: "Landwirtschaft",
+                value: "agriculture",
+            },
+            {
+                label: "Abfallentsorgung",
+                value: "waste",
+            },
+        ]
 
-  async updateMode() {
-    //setMode("shuffle")
-    console.log("update mode", this.state.landkreisSelection);
-    //return this.setStateAsync({mode:"shuffle"})
-    if (this.state.landkreisSelection.length > 1) {
-      return this.setState({ mode: "comparison" });
-    } else if (this.state.landkreisSelection.length === 1) {
-      return this.setStateAsync({ mode: "lk" });
-      // } else if (this.state.landkreisSelection.length === 2) {
-      //   //later this will be on click on a postcard
-      //   mode = "postcard";
-    } else {
-      return this.setStateAsync({ mode: "shuffle" });
-      //setMode("shuffle")
+        this.switchToPostcardView = this.switchToPostcardView.bind(this);
+        this.nextCard = this.nextCard.bind(this);
+        this.closePostcardView = this.closePostcardView.bind(this);
+
+        this.state = {
+            shuffleSelection: [
+                { lk: { value: "11" }, section: "mobility" },
+                { lk: "1011", section: "waste" },
+                { lk: "11", section: "mobility" },
+            ],
+            section: [],
+            landkreisSelection: [],
+            postcardView: false,
+            mode: "",
+            cardSelection: [],
+            activeCard: 0
+        };
     }
-    //console.log("updated mode:", this.state.mode)
 
-    //check if for mistakes during setting the mode
-    // if (!["comparison", "shuffle", "lk", "postcard"].includes(mode)) {
-    //     mode = "shuffle"
-    // }
-
-    // return mode;
-  }
-
-  switchToPostcardView() {
-    console.log("set postcard view true");
-    this.setState({ postcardView: true });
-  }
-
-  async changeLandkreis(e) {
-    // await this.setStateAsync({landkreisSelection:e})
-    // .then(()=>{this.updateCardSelection()})
-    this.setState({ landkreisSelection: e }, () => {
-      console.log("set state done");
-      this.updateCardSelection();
-    });
-  }
-
-  async changeSection(e) {
-    await this.setStateAsync({ section: e }).then(() => {
-      this.updateCardSelection();
-    });
-  }
-
-  async updateCardSelection() {
-    await this.updateMode().then(() => {
-      console.log("update mode done");
-      let list = [];
-      console.log(
-        "updateCardSelection postcardview? ",
-        this.state.postcardView
-      );
-      if (this.state.mode === "shuffle") {
-        console.log(this.state.mode, list);
-        list = this.state.shuffleSelection;
-      } else if (this.state.mode === "lk") {
-        list = [];
-        let selectedLK;
-        // set default value for landkreisSelection (TODO: use germany)
-        if (this.state.landkreisSelection[0] === undefined)
-          selectedLK = { value: "11", label: "Berlin" };
-        else {
-          selectedLK = {
-            value: this.state.landkreisSelection[0].value,
-            label: this.state.landkreisSelection[0].label,
-          };
-        } //set selected value for landkreisSelection
-
-        //add one card per section
-        this.state.sections.forEach((element) => {
-          list.push({ lk: selectedLK, section: element.value });
+    setStateAsync(state) {
+        return new Promise((resolve) => {
+            this.setState(state, resolve);
         });
-      } else if (this.state.mode === "comparison") {
-        let selectedSection;
+    }
 
-        // set default value for section
-        if (this.state.section === undefined) {
-          selectedSection = "energy";
-        }
-        //set selected value for section
-        else {
-          selectedSection = this.state.section.value;
+    async updateMode() {
+        //setMode("shuffle")
+        console.log("update mode", this.state.landkreisSelection);
+        //return this.setStateAsync({mode:"shuffle"})
+        if (this.state.landkreisSelection.length > 1) {
+            return this.setState({ mode: "comparison" });
         }
 
-        //add one card per landkreisSelection
-        this.state.landkreisSelection.forEach((element) => {
-          list.push({
-            lk: { value: element.value, label: element.label },
-            section: selectedSection,
-          });
+        else if (this.state.landkreisSelection.length === 1) {
+            return this.setStateAsync({ mode: "lk" });
+        }
+
+        else {
+            return this.setStateAsync({ mode: "shuffle" });
+        }
+
+        // TODO: check if for mistakes during setting the mode
+        // if (!["comparison", "shuffle", "lk", "postcard"].includes(mode)) {
+        //     mode = "shuffle"
+        // }
+
+        // return mode;
+    }
+
+    switchToPostcardView() {
+        console.log("set postcard view true");
+        this.setState({ postcardView: true });
+    }
+
+    closePostcardView() {
+        if(!this.state.postcardView) return
+
+        console.log("set postcard view false");
+        this.setState({ postcardView: false });
+    }
+
+    nextCard(){
+        if(!this.state.postcardView) return
+
+        //switch to next card in postcard view
+        let newActiveCard = (this.state.activeCard + 1) % this.state.cardSelection.length      
+        this.setState({activeCard: newActiveCard});
+        //setActiveCard((activeCard + 1) % cards().length);
+
+    }
+
+    async changeLandkreis(e) {
+        // await this.setStateAsync({landkreisSelection:e})
+        // .then(()=>{this.updateCardSelection()})
+        this.setState({ landkreisSelection: e }, () => {
+            this.updateCardSelection();
         });
-      }
+    }
 
-      console.log(this.state.mode, list);
-      this.setState({ cardSelection: list });
-      //return list;
-    });
-  }
+    async changeSection(e) {
+        await this.setStateAsync({ section: e }).then(() => {
+            this.updateCardSelection();
+        });
+    }
 
-  render() {
-    return (
-      <div>
-        <h2>Card Collections</h2>
+    async updateCardSelection() {
+        await this.updateMode().then(() => {
+            let list = [];
+            console.log(
+                "updateCardSelection postcardview? ",
+                this.state.postcardView
+            );
+            if (this.state.mode === "shuffle") {
+                console.log(this.state.mode, list);
+                list = this.state.shuffleSelection;
+            }
+            else if (this.state.mode === "lk") {
+                list = [];
+                let selectedLK;
+                // set default value for landkreisSelection (TODO: use germany)
+                if (this.state.landkreisSelection[0] === undefined) {
+                    selectedLK = { value: "11", label: "Berlin" };
+                }
+                else {
+                    selectedLK = {
+                        value: this.state.landkreisSelection[0].value,
+                        label: this.state.landkreisSelection[0].label,
+                    };
+                } //set selected value for landkreisSelection
 
-        <Select
-          isMulti
-          defaultValue={this.state.landkreisSelection}
-          onChange={this.changeLandkreis.bind(this)}
-          options={this.state.landkreise}
-        />
+                //add one card per section
+                this.sections.forEach((element) => {
+                    list.push({ lk: selectedLK, section: element.value });
+                });
+            }
+            else if (this.state.mode === "comparison") {
+                let selectedSection;
 
-        <h5>
-          Du hast{" "}
-          {this.state.landkreisSelection.map((elem) => elem.label + " ")}{" "}
-          ausgewählt.
-        </h5>
+                // set default value for section
+                if (this.state.section === undefined) {
+                    selectedSection = "energy";
+                }
+                //set selected value for section
+                else {
+                    selectedSection = this.state.section.value;
+                }
 
-        <div
-          style={{
-            visibility: this.state.mode === "comparison" ? "visible" : "hidden",
-          }}
-        >
-          <Select
-            defaultValue={this.state.section[0]}
-            onChange={this.changeSection.bind(this)}
-            options={this.state.sections}
-          />
+                //add one card per landkreisSelection
+                this.state.landkreisSelection.forEach((element) => {
+                    list.push({
+                        lk: { value: element.value, label: element.label },
+                        section: selectedSection,
+                    });
+                });
+            }
 
-          <h5>in der Sektion {this.state.section.label}.</h5>
-        </div>
+            console.log(this.state.mode, list);
+            this.setState({ cardSelection: list });
+            //return list;
+        });
+    }
 
-        <CardCollection
-          cardSelection={this.state.cardSelection}
-          mode={this.state.mode}
-          postcardView={this.state.postcardView}
-          switchToPostcardView={this.switchToPostcardView}
-        />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <h2>Card Collections</h2>
+
+                <Select
+                    isMulti
+                    defaultValue={this.state.landkreisSelection}
+                    onChange={this.changeLandkreis.bind(this)}
+                    options={this.landkreise}
+                    isOptionDisabled={() => this.state.landkreisSelection.length >= 3} //max selection number: 3
+                />
+
+                <h5>
+                    Du hast{" "}
+                    {this.state.landkreisSelection.map((elem) => elem.label + " ")}{" "}
+                    ausgewählt.
+                </h5>
+
+                <div
+                    style={{
+                        visibility: this.state.mode === "comparison" ? "visible" : "hidden",
+                    }}
+                >
+                    <Select
+                        defaultValue={this.state.section[0]}
+                        onChange={this.changeSection.bind(this)}
+                        options={this.sections}
+                    />
+
+                    <h5>in der Sektion {this.state.section.label}.</h5>
+                </div>
+
+                <CardCollection
+                    cardSelection={this.state.cardSelection}
+                    mode={this.state.mode}
+                    postcardView={this.state.postcardView}
+                    activeCard = {this.state.activeCard}
+                    switchToPostcardView={this.switchToPostcardView.bind(this)}
+                />
+
+                {this.state.postcardView && <div className="button-container">
+                    <button className="close-button" onClick={this.closePostcardView}>
+                        <img
+                            src={close}
+                            className="close-button-img"
+                            alt="close-button-img"
+                        />
+                    </button>
+
+                    <button className="switch-button" onClick={this.nextCard}>
+                        <img
+                            src={switchCard}
+                            className="switch-button-img"
+                            alt="switch-button-img"
+                        />
+                    </button>
+
+                    {/* <button className="flip-button">
+                        <img src={flip} className="flip-button-img" alt="flip-button-img" />
+                    </button> */}
+                </div>}
+            </div>
+        );
+    }
 }
