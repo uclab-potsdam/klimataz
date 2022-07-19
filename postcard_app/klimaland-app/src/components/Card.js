1a238a65e104164d9276d2b4188b3602eeff67dc
aeschi <anna.eschenbacher@icloud.com>
2022-07-19 12:03:45 +0200

    changes flipping

--- postcard_app/klimaland-app/src/components/Card.js
+++ postcard_app/klimaland-app/src/components/Card.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Side from "./Side";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import flip from "../img/buttons/flip.png";
 import { mod } from "./helper";
 
@@ -10,25 +10,47 @@ const Card = ({ classProp, lk, section, clickOnCard }) => {
     // clickOnCard();
   };
 
+  const sides = ["side 1", "side 2", "side 3", "side 4"];
+
   const [activeSide, setActiveSide] = useState(0);
 
+  let index = 0;
+  const flipped = useRef(0);
+
   useEffect(() => {
-    console.log("flip card: " + activeSide);
+    index = mod(activeSide, sides.length);
+    flipped.current = !flipped;
+    console.log("side index: " + index);
+    console.log("flipped: " + flipped);
   }, [activeSide]);
 
   return (
     // <div className={classProp} onClick={handleClick}>
     <div className={classProp}>
-      {/* <Side lk={lk} section={section} /> */}
-
-      <button
-        className="flip-button"
-        onClick={() => {
-          setActiveSide(activeSide + 1);
-        }}
-      >
-        <img src={flip} className="flip-button-img" alt="flip-button-img" />
-      </button>
+      <div className={`side-container ${flipped ? "flip" : ""}`}>
+        <div className="card-front">
+          <Side lk={lk} section={section} />
+          <button
+            className="flip-button"
+            onClick={() => {
+              setActiveSide(activeSide + 1);
+            }}
+          >
+            <img src={flip} className="flip-button-img" alt="flip-button-img" />
+          </button>
+        </div>
+        <div className="card-back">
+          <Side lk={lk} section={section} />
+          <button
+            className="flip-button"
+            onClick={() => {
+              setActiveSide(activeSide + 1);
+            }}
+          >
+            <img src={flip} className="flip-button-img" alt="flip-button-img" />
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
