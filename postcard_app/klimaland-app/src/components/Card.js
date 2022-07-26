acb6f27943159a28c24bd7e6e883a0f50b18ae89
aeschi <anna.eschenbacher@icloud.com>
2022-07-26 17:46:33 +0200

    infinite flip of cards

--- postcard_app/klimaland-app/src/components/Card.js
+++ postcard_app/klimaland-app/src/components/Card.js
@@ -6,51 +6,85 @@ import LayoutControls from "../data/layout-controls.json";
 import flip from "../img/buttons/flip.png";
 import { mod } from "./helper";
 
-const Card = ({ classProp, lk, section, clickOnCard, isThumbnail, isTopCard }) => {
+const Card = ({
+  classProp,
+  lk,
+  section,
+  clickOnCard,
+  isThumbnail,
+  isTopCard,
+}) => {
   //const sides = ["side 1", "side 2", "side 3", "side 4"];
   //const sides = ["side 1", "side 2", "side 3"];
 
   const sides = LayoutControls[section].params;
 
-  console.log(sides)
+  // console.log(sides);
 
   const [activeSide, setActiveSide] = useState(0);
   const [flipped, setFlipped] = useState(0);
 
   const layoutControls = LayoutControls[section].params;
 
-  let index = 0;
-
   //TODO: flip first, after that switch the content on the side
   //maybe with gsap??
 
   useEffect(() => {
-    index = mod(activeSide, sides.length);
     const currentVal = flipped ? false : true;
     setFlipped(currentVal);
   }, [activeSide]);
 
   const renderSide = function (cardSide) {
-    return (
-      <div className={cardSide}>
-        <Side
-          lk={lk}
-          section={section}
-          isThumbnail={isThumbnail}
-          isTopCard={isTopCard}
-          activeSide={mod(activeSide, sides.length)}
-          layoutControls = {sides}
-        />
-        <button
-          className="button flip"
-          onClick={() => {
-            setActiveSide(activeSide + 1);
-          }}
-        >
-          <img src={flip} className="button img" alt="flip-button-img" />
-        </button>
-      </div>)
-  }
+    if (cardSide === "card-front") {
+      return (
+        <div className={cardSide}>
+          <Side
+            lk={lk}
+            section={section}
+            isThumbnail={isThumbnail}
+            isTopCard={isTopCard}
+            activeSide={mod(activeSide, sides.length)}
+            layoutControls={sides}
+            style={{
+              transform: "rotateY(" + activeSide * 180 - 180 + "deg)",
+            }}
+          />
+          <button
+            className="button flip"
+            onClick={() => {
+              setActiveSide(activeSide + 1);
+            }}
+          >
+            <img src={flip} className="button img" alt="flip-button-img" />
+          </button>
+        </div>
+      );
+    } else {
+      return (
+        <div className={cardSide}>
+          <Side
+            lk={lk}
+            section={section}
+            isThumbnail={isThumbnail}
+            isTopCard={isTopCard}
+            activeSide={mod(activeSide, sides.length)}
+            layoutControls={sides}
+            style={{
+              transform: "rotateY(" + activeSide * 180 + "deg)",
+            }}
+          />
+          <button
+            className="button flip"
+            onClick={() => {
+              setActiveSide(activeSide + 1);
+            }}
+          >
+            <img src={flip} className="button img" alt="flip-button-img" />
+          </button>
+        </div>
+      );
+    }
+  };
 
   return (
     <div className={classProp}>
@@ -60,14 +94,19 @@ const Card = ({ classProp, lk, section, clickOnCard, isThumbnail, isTopCard }) =
             lk={lk}
             section={section}
             isThumbnail={isThumbnail}
-            layoutControls = {sides}
+            // layoutControls={sides}
             activeSide={0} //active side for thumbnail always first one
-            layoutControls = {layoutControls}
+            layoutControls={layoutControls}
           />
         </div>
       )}
       {!isThumbnail && (
-        <div className={`side-container ${flipped ? "flip" : ""}`}>
+        <div
+          className={`side-container ${flipped ? "flip" : ""}`}
+          style={{
+            transform: "rotateY(" + activeSide * 180 + "deg)",
+          }}
+        >
           {renderSide("card-front")}
           {renderSide("card-back")}
         </div>
