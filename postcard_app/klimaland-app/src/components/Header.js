fadcd04483fc28f21be19733103707859a928b83
aeschi <anna.eschenbacher@icloud.com>
2022-07-29 11:46:43 +0200

    taz header integration
    - added taz header
    - made everything responsive

---
+++ postcard_app/klimaland-app/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import headertaz from "../img/taz/header_taz.png";
+
+const Header = () => {
+  return (
+    <div>
+      <img src={headertaz} className="header-taz" alt="header-taz" />
+    </div>
+  );
+};
+
+export default Header;
