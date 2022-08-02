2b0c0141a1d65d3a804bf3e8b3b8e8ca5bae836c
more-ginger <francesca1morini@gmail.com>
2022-08-02 17:37:15 +0200

    init design of back-side #13

--- postcard_app/klimaland-app/src/components/Side.js
+++ postcard_app/klimaland-app/src/components/Side.js
@@ -3,12 +3,10 @@ import { setStateAsync } from "./helperFunc.js";
 //chart container
 import Chart from './side-elements/Chart.js';
 //side elements
-import Text from "./side-elements/Text.js";
-import Locator from "./side-elements/Locator.js";
-
+import Details from './side-elements/Details.js'
 export default class Side extends Component {
-  constructor(props) {
-    super(props);
+   constructor(props) {
+      super(props);
 
       let layoutCombo = this.props.layoutControls[this.props.activeSide][this.props.activeSide].combo
 
@@ -37,23 +35,23 @@ export default class Side extends Component {
          let cardwidth = this.props.windowSize.width / 10 * 3 //30vw postcard size in thumbnail
          let width = cardwidth - (cardwidth / 3) //conditional margin
          let height = width * 0.7 //fixed ratio for postcard look
-         await setStateAsync(this,{ chartStyle: { width: String(width), height: String(height) } })
+         await setStateAsync(this, { chartStyle: { width: String(width), height: String(height) } })
       }
 
       else {
          let cardwidth = this.props.windowSize.width / 10 * 7 //70vw postcard size in fullscreen
          let width = cardwidth - (cardwidth / 4) //conditional margin
          let height = width * 0.7 //fixed ratio for postcard look
-         await setStateAsync(this,{ chartStyle: { width: String(width), height: String(height) } })
+         await setStateAsync(this, { chartStyle: { width: String(width), height: String(height) } })
       }
    }
 
    //load component dynamically from vis index file
-   vis(){
-      if(this.props.layoutControls[this.props.activeSide][0].components !== undefined){         
+   vis() {
+      if (this.props.layoutControls[this.props.activeSide][0].components !== undefined) {
          return <Chart {...this.props} />
       }
-      else{
+      else {
          //if not specified yet: return nothing
          return
       }
@@ -69,7 +67,7 @@ export default class Side extends Component {
          //only for top card because of performance
          if (this.props.isTopCard) {
             let layoutCombo = this.props.layoutControls[this.props.activeSide][this.props.activeSide].combo
-            await setStateAsync(this,{ order: layoutCombo[0], showViz: layoutCombo[1], indicator: layoutCombo[2], showLocator: layoutCombo[3] })
+            await setStateAsync(this, { order: layoutCombo[0], showViz: layoutCombo[1], indicator: layoutCombo[2], showLocator: layoutCombo[3] })
          }
 
          await this.updateChartSize();
@@ -82,17 +80,15 @@ export default class Side extends Component {
 
    render() {
       return (
-         <div className="side-inner" onClick={(e) => this.props.clickOnCard(e, this.props.lk, this.props.section)}>
+         <div
+            className="side-inner"
+            onClick={(e) => this.props.clickOnCard(e, this.props.lk, this.props.section)}
+         >
             {!this.state.showViz && //TEXT
-               <Text
-                  lk={this.props.lk}
-                  section={this.props.section}
-                  activeSide={this.props.activeSide} />}
+               <Details lk={this.props.lk} section={this.props.section} activeSide={this.props.activeSide} />
+            }
             {this.state.showViz &&  //VIS
                this.props.activeSide === 0 && this.vis()}
-            {this.state.showLocator && //LOCATOR
-               <Locator
-                  lk={this.props.lk} />}
          </div>
       )
    }
