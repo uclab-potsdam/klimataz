2a529ba12bbf0a2ab178dc8bff97d25cc3422b4a
johannahaha <johanna.hartmann@filmuniversitaet.de>
2022-08-03 12:28:37 +0200

    #54 switch cards back and forth

--- postcard_app/klimaland-app/src/components/Card.js
+++ postcard_app/klimaland-app/src/components/Card.js
@@ -1,14 +1,20 @@
-import React from "react";
-import { useState, useEffect } from "react";
-import { useSwipeable } from "react-swipeable";
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { useSwipeable } from 'react-swipeable';
 
-import flipCard from "../img/buttons/flip.svg";
-import { mod } from "./helperFunc";
+import flipCard from '../img/buttons/flip.svg';
+import { mod } from './helperFunc';
 
-const Card = ({ classProp, sides, isThumbnail, children,nextCard }) => {
+const Card = ({
+  classProp,
+  sides,
+  isThumbnail,
+  children,
+  handleSwitchBack,
+  handleSwitchNext,
+}) => {
   const [activeSide, setActiveSide] = useState(0);
   const [flipped, setFlipped] = useState(0);
-
 
   //TODO: flip first, after that switch the content on the side
   //maybe with gsap??
@@ -19,9 +25,12 @@ const Card = ({ classProp, sides, isThumbnail, children,nextCard }) => {
   }, [activeSide]);
 
   const swipeHandler = useSwipeable({
-    onSwiped: () => {
-      nextCard();
-    }
+    onSwipedLeft: () => {
+      handleSwitchBack();
+    },
+    onSwipedRight: () => {
+      handleSwitchNext();
+    },
   });
 
   const sideWithProps = function (rotation) {
@@ -38,13 +47,13 @@ const Card = ({ classProp, sides, isThumbnail, children,nextCard }) => {
 
   const renderSide = function (cardSide) {
     let rotation;
-    if (cardSide === "card-front") {
-      rotation = { transform: "rotateY(" + activeSide * 180 - 180 + "deg)" };
-    } else if (cardSide === "card-back") {
-      rotation = { transform: "rotateY(" + activeSide * 180 + "deg)" };
+    if (cardSide === 'card-front') {
+      rotation = { transform: 'rotateY(' + activeSide * 180 - 180 + 'deg)' };
+    } else if (cardSide === 'card-back') {
+      rotation = { transform: 'rotateY(' + activeSide * 180 + 'deg)' };
     } else {
       //card-preview
-      console.log("card class for rotation is not set");
+      console.log('card class for rotation is not set');
     }
 
     return (
@@ -63,23 +72,26 @@ const Card = ({ classProp, sides, isThumbnail, children,nextCard }) => {
   };
 
   //naive calc for initial small rotation
-  const ranNum = Math.random() * (Math.round(Math.random()) ? 1 : -1)
-  const previewRotation = "rotate(" + ranNum * 5 + "deg)"
+  const ranNum = Math.random() * (Math.round(Math.random()) ? 1 : -1);
+  const previewRotation = 'rotate(' + ranNum * 5 + 'deg)';
 
   return (
     <div className={classProp}>
-      {isThumbnail && <div className="card-preview" 
-      style={{transform:previewRotation}}>{sideWithProps({})}</div>}
+      {isThumbnail && (
+        <div className="card-preview" style={{ transform: previewRotation }}>
+          {sideWithProps({})}
+        </div>
+      )}
       {!isThumbnail && (
         <div
-          className={`side-container ${flipped ? "flip" : ""}`}
+          className={`side-container ${flipped ? 'flip' : ''}`}
           style={{
-            transform: "rotateY(" + activeSide * 180 + "deg)",
+            transform: 'rotateY(' + activeSide * 180 + 'deg)',
           }}
           {...swipeHandler}
         >
-          {renderSide("card-front")}
-          {renderSide("card-back")}
+          {renderSide('card-front')}
+          {renderSide('card-back')}
         </div>
       )}
     </div>
