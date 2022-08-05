efa203a6c119917fb7ed6a79e0568fe59d92a1a4
more-ginger <francesca1morini@gmail.com>
2022-08-05 11:39:12 +0200

    add dummy iframe in index.html

--- postcard_app/klimaland-app/src/App.js
+++ postcard_app/klimaland-app/src/App.js
@@ -1,11 +1,9 @@
 import Canvas from "./components/Canvas";
 import "./styles/main.scss";
-import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-      <Header />
       <Canvas />
     </div>
   );
