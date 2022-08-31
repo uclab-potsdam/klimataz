84b8737115301baac9cd5b7c631092499a791222
more-ginger <francesca1morini@gmail.com>
2022-08-31 15:21:51 +0200

    algorithm for km vis #98

--- klimaland-app/src/components/side-elements/vis/MoModalSplit.js
+++ klimaland-app/src/components/side-elements/vis/MoModalSplit.js
@@ -23,46 +23,78 @@ const MoModalSplit = ({ currentData, currentIndicator, currentSection, lkData, i
     const marginWidth = Math.round(dimensions.width / 20)
     const rightMarginWidth = Math.round(dimensions.width - dimensions.width / 3)
     const orderOfModes = ['Fuß', 'Fahrrad', 'ÖPV', 'Mitfahrer', 'Fahrer']
+    const colors = ['#3762FB', '#2A4D9C', '#5F88C6', '#FFD0D0', '#FF7B7B']
     let yScale;
-
-    // console.log(lkData, currentData)
+    let xScale;
+    let xScaleReverse;
 
     if (currentData.data !== undefined) {
         averageKmDistance = currentData.data.filter(d => d.column.includes('average') && +d.year === defaultYear)
-        console.log(averageKmDistance)
         const longestAvgRoute = max(averageKmDistance.map(d => d.value))
         const maxYValue = Math.floor(longestAvgRoute / 10)
-        const xScale = scaleLinear().domain([0, 10]).range([marginWidth, rightMarginWidth])
-        yScale = scaleLinear().domain([0, maxYValue]).range([0, dimensions.height / 15])
-        const lineConstructor = line().x(d => xScale(d[0])).y(d => yScale(d[1]))
+        xScale = scaleLinear().domain([0, 10]).range([marginWidth, rightMarginWidth])
+        xScaleReverse = scaleLinear().domain([10, 0]).range([marginWidth, rightMarginWidth])
+        yScale = scaleLinear().domain([0, maxYValue]).range([0, dimensions.height / 10])
 
         averageKmDistance.forEach((mode, m) => {
+
             const element = {}
             const nameOfMode = mode.column.split("_").pop()
             element.mode = nameOfMode.replace(/[{()}]/g, '')
-            element.label = mode.value.toFixed(2)
-            element.values = [[0, 0]]
-            const valueMagnitude = Math.floor(mode.value / 10)
-            const difference = mode.value - valueMagnitude * 10
+            element.label = mode.value.toFixed(2) + 'km'
 
-            if (valueMagnitude > 1) {
-                let columnIndex = 0
-                for (let index = 1; index <= valueMagnitude; index++) {
-                    element.values[index] = [10, columnIndex]
-                    if (index % 2 !== 0) {
-                        columnIndex = columnIndex + 1
-                    }
+            // depending on size of value start at zero (for > 10) or with value (< 10)
+            const startValue = 0.1
+            const values = mode.value > 10 ? [] : [[0, 0]]
+            let difference = mode.value > 10 ? 0 : mode.value
+
+            // for elements bigger than 10 loops over and push coordinates
+            let tot = mode.value
+            let rowIndex = 0
+            let i = 10
+
+            while (i < tot) {
+                difference = tot - i
+                // if it's even rows goes from left to right
+                if (rowIndex % 2 === 0) {
+                    values.push(
+                        [0, rowIndex],
+                        [10, rowIndex],
+                        [10, rowIndex + 1]
+                    )
+                } else {
+                    // if it's odd rows goes from right to left
+                    values.push(
+                        [10, rowIndex],
+                        [startValue, rowIndex],
+                        [startValue, rowIndex + 1]
+                    )
                 }
 
-                if (difference > 0) {
-                    element.values.push([difference, columnIndex])
-                }
-
-            } else {
-                element.values.push([mode.value, 0])
+                rowIndex = rowIndex + 1
+                i += 10
             }
 
-            element.path = lineConstructor(element.values)
+            // constructor is created on the fly to reverse scale
+            const lineConstructor = line()
+                .x(d => (d[0] !== startValue && d[0] !== 0) && d[0] < 5 && rowIndex > 0
+                    ? xScaleReverse(d[0])
+                    : xScale(d[0]))
+                .y(d => yScale(d[1]))
+
+            // pushing last element with difference value, 
+            //in items < 10 this should be = tot
+            const lastCoor = [difference, rowIndex]
+            values.push(lastCoor)
+            // create path
+            element.path = lineConstructor(values)
+
+            // accessory elements
+            element.labelX = lastCoor[0] !== 0 && lastCoor[0] < 5 && rowIndex > 0
+                ? xScaleReverse(lastCoor[0])
+                : xScale(lastCoor[0])
+            element.labelY = yScale(lastCoor[1])
+
             plotAvgData.push(element)
         })
 
@@ -74,7 +106,8 @@ const MoModalSplit = ({ currentData, currentIndicator, currentSection, lkData, i
             <div className="description">
                 <div className="title">
                     <h3>
-                        Mit was und wie weit fahren Menschen in <span>{lkData}</span> zur Arbeit?
+                        Mit was und wie weit fahren Menschen
+                        in <span>{lkData}</span> zur Arbeit?
                     </h3>
                 </div>
             </div>
@@ -83,9 +116,31 @@ const MoModalSplit = ({ currentData, currentIndicator, currentSection, lkData, i
                     {
                         plotAvgData.map((trip, t) => {
                             return (
-                                <g transform={`translate(0, ${(t + 0.5) * 60})`} key={t} className={trip.mode}>
+                                <g
+                                    transform={`translate(0, ${(t + 0.5) * 60})`}
+                                    key={t}
+                                    className={trip.mode}
+                                >
                                     <text x={marginWidth} y="-8">{trip.mode}</text>
-                                    <path d={trip.path} stroke="black" fill="none" stroke-width="5" />
+                                    <path
+                                        d={trip.path}
+                                        stroke={colors[t]}
+                                        fill="none"
+                                        stroke-width="10"
+                                    />
+                                    <g
+                                        className="avgkm-marker"
+                                        transform={`translate(${trip.labelX}, ${trip.labelY + 5})`}
+                                    >
+                                        <text x="5" y="-20">{trip.label}</text>
+                                        <line
+                                            x1="0"
+                                            x2="0"
+                                            y1="0"
+                                            y2="-30"
+                                            stroke="black"
+                                        />
+                                    </g>
                                 </g>
                             )
                         })
