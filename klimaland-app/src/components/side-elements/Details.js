34c93de0ba4e55181dc697a87095e0805d667c82
more-ginger <francesca1morini@gmail.com>
2022-09-15 17:34:18 +0200

    update list, render custom texts from data source

--- klimaland-app/src/components/side-elements/Details.js
+++ klimaland-app/src/components/side-elements/Details.js
@@ -2,21 +2,57 @@ import React, { Component } from 'react';
 import Text from './Text.js';
 import Locator from './Locator.js';
 import List from './List.js';
+import { readString } from 'react-papaparse';
+
+import DynamicText from '../../data/final_postcard_texts.csv';
+
+let DynamicTextData
+const papaConfig = {
+  header: true,
+  complete: (results, file) => {
+    // console.log('Parsing complete:', results, file);
+    DynamicTextData = results.data
+  },
+  download: true,
+  error: (error, file) => {
+    console.log('Error while parsing:', error, file);
+    DynamicTextData = []
+  },
+};
+
+readString(DynamicText, papaConfig);
 
 export default class Details extends Component {
   render() {
+
+    const sectionFullName = {
+      La: { de: "Landwirtschaft", en: "agriculture" },
+      Mo: { de: "Mobilität", en: "mobility" },
+      Ge: { de: "Gebäude", en: "buildings" },
+      En: { de: "Energie", en: "energy" },
+      Ab: { de: "Abfall", en: "waste" }
+    }
+
     return (
       <div className="details-container">
         <div className="flex-container">
           <div className="text-container">
-            <Text {...this.props} />
+            <Text
+              {...this.props}
+              sectionFullName={sectionFullName}
+              data={DynamicTextData}
+            />
           </div>
           <div className="data-container">
             <div className="locator-map">
               <Locator lk={this.props.lk} />
             </div>
             <div className="lk-list">
-              <List />
+              <List
+                {...this.props}
+                sectionFullName={sectionFullName}
+                data={DynamicTextData}
+              />
             </div>
           </div>
         </div>
--- klimaland-app/src/components/side-elements/List.js
+++ klimaland-app/src/components/side-elements/List.js
@@ -1,25 +1,36 @@
 import React from "react";
 
-const List = () => {
+const List = ({ lk, data, section, sectionFullName }) => {
+    const agsCurrentCard = data.filter(d => { return +d.AGS === lk.value })
+    const thirdKey = sectionFullName[section].en + '_third'
+
+    // pulling similar lks (within the bl)
+    const similarAgs = data.filter(d => {
+        return d[thirdKey] === agsCurrentCard[0][thirdKey]
+            // && d.AGS.substring(0, 2) === agsCurrentCard[0].AGS.substring(0, 2)
+            && +d.AGS !== lk.value
+    })
+
+    // shuffle the array
+    const shuffled = similarAgs.sort(() => 0.5 - Math.random());
+    // get 10 random location
+    const randomSample = shuffled.slice(0, 10);
+    const agsRanking = agsCurrentCard[0][thirdKey].substring(0, agsCurrentCard[0][thirdKey].indexOf(' '))
+
     return (
         <div className="list-inner-container">
-            <div className="list-similar-container">
-                <h5>Similar</h5>
+            <div className={`list-similar-container ${agsRanking}`}>
+                <h5>{agsCurrentCard[0][thirdKey]} wie:</h5>
                 <ul>
-                    <ol>München</ol>
-                    <ol>Barnim</ol>
-                    <ol>Hamburg</ol>
-                    <ol>Freiburg</ol>
-                    <ol>Köln (Landkreis)</ol>
-                </ul>
-            </div>
-            <div className="list-dissimilar-container">
-                <h5>Different</h5>
-                <ul>
-                    <ol>Thüringen (Bundesland)</ol>
-                    <ol>Schweinfurt</ol>
-                    <ol>this name is really long</ol>
-                    <ol>Berlin (Stadt)</ol>
+                    {
+                        randomSample.map((ags, a) => {
+                            return (
+                                <ol key={a}>
+                                    {ags.Name}
+                                </ol>
+                            )
+                        })
+                    }
                 </ul>
             </div>
         </div>
