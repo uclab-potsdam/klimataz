56b2132424f5524cc2214a1ef050a8f20f779734
aeschi <anna.eschenbacher@icloud.com>
2022-09-16 18:11:46 +0200

    taking out console log

--- klimaland-app/src/components/CardCollection.js
+++ klimaland-app/src/components/CardCollection.js
@@ -25,8 +25,6 @@ export default class CardCollection extends Component {
       },
     };
 
-    console.log(this.state)
-
     // readString(DynamicText, papaConfig)
 
     //load all the data
@@ -49,7 +47,9 @@ export default class CardCollection extends Component {
       download: true,
       skipEmptyLines: true,
       // Here this is also available. So we can call our custom class method
-      complete: (results, file) => { this.updateData(results) }
+      complete: (results, file) => {
+        this.updateData(results);
+      },
     });
   }
 
@@ -59,7 +59,6 @@ export default class CardCollection extends Component {
     // Here this is available and we can call this.setState (since it's binded in the constructor)
     this.setState({ textData: data }); // or shorter ES syntax: this.setState({ data });
   }
-
 
   /**
    * calls switchToPostcardView in LayoutControls
