bf8e64c0857e98789813e9d2283bcab4d5f366a3
aeschi <anna.eschenbacher@icloud.com>
2022-09-16 11:28:34 +0200

    minor text adjustments

--- klimaland-app/src/components/side-elements/vis/MoModalSplit.js
+++ klimaland-app/src/components/side-elements/vis/MoModalSplit.js
@@ -2,7 +2,13 @@ import React, { useRef, useLayoutEffect, useState } from 'react';
 import { scaleLinear } from 'd3-scale';
 import { line, stack } from 'd3-shape';
 
-const MoModalSplit = ({ currentData, currentIndicator, currentSection, locationLabel, isThumbnail }) => {
+const MoModalSplit = ({
+  currentData,
+  currentIndicator,
+  currentSection,
+  locationLabel,
+  isThumbnail,
+}) => {
   const targetRef = useRef();
   const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
 
@@ -17,7 +23,17 @@ const MoModalSplit = ({ currentData, currentIndicator, currentSection, locationL
 
   window.mobileCheck = function () {
     let check = false;
-    (function (a) { if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true; })(navigator.userAgent || navigator.vendor || window.opera);
+    (function (a) {
+      if (
+        /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(
+          a
+        ) ||
+        /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(
+          a.substr(0, 4)
+        )
+      )
+        check = true;
+    })(navigator.userAgent || navigator.vendor || window.opera);
     return check;
   };
 
@@ -25,9 +41,9 @@ const MoModalSplit = ({ currentData, currentIndicator, currentSection, locationL
   let plotAvgData = [];
   let plotPercData = [];
   let defaultYear = 2017;
-  const width = dimensions.width
-  const height = dimensions.height
-  const isMobile = width <= 350 && window.mobileCheck(window)
+  const width = dimensions.width;
+  const height = dimensions.height;
+  const isMobile = width <= 350 && window.mobileCheck(window);
   const mobileThreshold = isMobile ? 10 : 4;
   const tabletThreshold = width >= 600 && width <= 750 ? 55 : 65;
   // let maxYValue = null;
@@ -52,7 +68,9 @@ const MoModalSplit = ({ currentData, currentIndicator, currentSection, locationL
 
     xScale = scaleLinear().domain([0, 10]).range([marginWidth, rightMarginWidth]);
     xScaleReverse = scaleLinear().domain([10, 0]).range([marginWidth, rightMarginWidth]);
-    yScale = scaleLinear().domain([0, 5]).range([0, height / 4.5]);
+    yScale = scaleLinear()
+      .domain([0, 5])
+      .range([0, height / 4.5]);
 
     averageKmDistance.forEach((mode, m) => {
       const element = {};
@@ -74,18 +92,10 @@ const MoModalSplit = ({ currentData, currentIndicator, currentSection, locationL
         difference = tot - i;
         // if it's even rows goes from left to right
         if (rowIndex % 2 === 0) {
-          values.push(
-            [0, rowIndex],
-            [10, rowIndex],
-            [10, rowIndex + 1]
-          );
+          values.push([0, rowIndex], [10, rowIndex], [10, rowIndex + 1]);
         } else {
           // if it's odd rows goes from right to left
-          values.push(
-            [10, rowIndex],
-            [startValue, rowIndex],
-            [startValue, rowIndex + 1]
-          );
+          values.push([10, rowIndex], [startValue, rowIndex], [startValue, rowIndex + 1]);
         }
 
         rowIndex = rowIndex + 1;
@@ -186,13 +196,17 @@ const MoModalSplit = ({ currentData, currentIndicator, currentSection, locationL
               <g className="paths">
                 <g className="main-x-axis" transform={`translate(${xScale(0)}, ${marginHeight})`}>
                   <line x1="0" x2={xScale(9.5)} y1="0" y2="0" />
-                  <text x="0" y="15">
+                  {/* <text x="0" y="15">
                     average Km
-                  </text>
+                  </text> */}
                 </g>
                 {plotAvgData.map((trip, t) => {
                   return (
-                    <g transform={`translate(0, ${(t + 0.5) * tabletThreshold})`} key={t} className={trip.mode}>
+                    <g
+                      transform={`translate(0, ${(t + 0.5) * tabletThreshold})`}
+                      key={t}
+                      className={trip.mode}
+                    >
                       <g className="axis">
                         <text x={marginWidth} y="-18" fill={colors[t]}>
                           {trip.mode}
@@ -238,14 +252,14 @@ const MoModalSplit = ({ currentData, currentIndicator, currentSection, locationL
             className="bar-percentage-trip"
             transform={`translate(${rightMarginWidth + marginWidth * 2}, 0)`}
           >
-            <text x="10" y={marginHeight + 15} textAnchor="end">
+            <text x="10" y={marginHeight + 15} textAnchor="middle">
               % der Trips
             </text>
             {plotPercData.map((trip, t) => {
               return (
                 <g transform={`translate(0, ${trip.y1})`} key={t}>
                   <text x="15" y="13" fill={trip.fill}>
-                    {trip.label}
+                    {trip.label}%
                   </text>
                   <rect width="10" height={trip.y2 - trip.y1} x="0" y="0" fill={trip.fill} />
                   <line x1="0" x2="30" y1="0" y2="0" />
