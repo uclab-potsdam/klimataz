60a5e5afe2811286cea4f81b25c95adf35552495
johannahaha <johanna.hartmann@filmuniversitaet.de>
2022-10-14 14:36:50 +0200

    color zoom map in color of ranking. #339

--- klimaland-app/src/components/side-elements/Details.js
+++ klimaland-app/src/components/side-elements/Details.js
@@ -50,7 +50,11 @@ export default class Details extends Component {
           </div>
           <div className="data-container">
             <div className="locator-map">
-              <Locator lk={this.props.lk} handleClickOnMap={this.handleClickOnList} />
+              <Locator
+                lk={this.props.lk}
+                handleClickOnMap={this.handleClickOnList}
+                section={this.props.section}
+              />
             </div>
             <div className="lk-list">
               <List
--- klimaland-app/src/components/side-elements/Locator.js
+++ klimaland-app/src/components/side-elements/Locator.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import { geoPath, geoMercator } from 'd3-geo';
 import LandkreiseOutline from '../../data/kreise-simpler.json';
 import bundeslaenderOutline from '../../data/bundeslaender.json';
-import { getTotalLKName, setStateAsync } from '../../helpers/helperFunc';
+import { getRanking, getTotalLKName, setStateAsync } from '../../helpers/helperFunc';
 import { UIContext } from '../UIContext';
 import DropDownControls from '../../data/selector-controls.json';
 
@@ -99,6 +99,7 @@ export default class Locator extends Component {
         translatedPath: geoTranslated(d),
         lk: d.properties.ARS,
         bl: d.properties.SN_L,
+        ranking: getRanking(parseInt(d.properties.ARS), this.props.section),
         visible:
           +this.props.lk.value === +d.properties.ARS ||
           +this.props.lk.value === +d.properties.SN_L ||
@@ -154,7 +155,6 @@ export default class Locator extends Component {
   handleClickOnMap(ags) {
     //return if ui vis = false
     if (!this.context) {
-      console.log('no ui');
       return;
     }
     //find lk in list of landkreise
@@ -162,6 +162,7 @@ export default class Locator extends Component {
     if (chosenLK !== undefined) {
       const lk = { value: ags, label: getTotalLKName(chosenLK) };
       this.props.handleClickOnMap(lk);
+      getRanking(ags, this.props.section);
     }
   }
 
@@ -185,7 +186,7 @@ export default class Locator extends Component {
                           id="map"
                           className={`landkreis ${el.lk} ${el.bl} ${
                             el.visible ? 'visible' : 'hidden'
-                          }`}
+                          } ${el.ranking}`}
                           onClick={this.handleClickOnMap.bind(this, parseInt(el.lk))}
                         />
                       );
