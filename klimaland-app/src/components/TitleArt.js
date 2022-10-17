4df3336191fd0a85c353ff86c5f1569a05493b15
aeschi <anna.eschenbacher@icloud.com>
2022-10-17 14:56:47 +0200

    change title

--- klimaland-app/src/components/TitleArt.js
+++ klimaland-app/src/components/TitleArt.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
 const TitleArt = ({ landkreisLabel }) => {
-    return (
-        <div className="word-art-title">
-            <h4 className="gruss-thumb" />
-            <h2 className="wordart main additional-0">{landkreisLabel}</h2>
-            {/* <h2 className="wordart additional-1">{landkreisLabel}</h2>
+  return (
+    <div className="word-art-title">
+      <h4 className="gruss-thumb" />
+      <h2 className="wordart main additional-0">{landkreisLabel + '?'}</h2>
+      {/* <h2 className="wordart additional-1">{landkreisLabel}</h2>
             <h2 className="wordart additional-2">{landkreisLabel}</h2>
             <h2 className="wordart additional-3">{landkreisLabel}</h2>
             <h2 className="wordart additional-3">{landkreisLabel}</h2> */}
-        </div>
-    )
+    </div>
+  );
 };
 
 export default TitleArt;
