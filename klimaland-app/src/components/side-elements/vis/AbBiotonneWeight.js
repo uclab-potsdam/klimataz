04b65ecae43179ef60f27aec02fc3c07b2adbd23
aeschi <anna.eschenbacher@icloud.com>
2022-10-25 11:18:46 +0200

    Update AbBiotonneWeight.js
    fix #502

--- klimaland-app/src/components/side-elements/vis/AbBiotonneWeight.js
+++ klimaland-app/src/components/side-elements/vis/AbBiotonneWeight.js
@@ -219,12 +219,16 @@ const Waste = ({ currentData, locationLabel, isThumbnail, footnote, cardNumber }
                     </g>
                   </g>
                 </g>
-                <g transform="translate(150, 0)">
-                  <circle className="highestValue" cx="0" cy="2" r={radius} />
-                  <text x={radius + 10} y={radius / 2}>
-                    Höchster Wert
-                  </text>
-                </g>
+              </g>
+              <g
+                transform={`translate(${width - (marginWidth - tabletThreshold) * 3.5 + 150}, ${
+                  marginHeight / 2
+                })`}
+              >
+                <circle className="highestValue" cx="0" cy="2" r={radius} />
+                <text x={radius + 10} y={radius / 2}>
+                  Höchster Wert
+                </text>
               </g>
             </g>
           )}
