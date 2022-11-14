daf4f434cf16b3d3d8f19992473a5585f91ce90d
aeschi <anna.eschenbacher@icloud.com>
2022-11-14 11:14:41 +0100

    logos added
    - delete header.js
    - delete header-taz-image
    - add fhp logo and taz logo solve #541

--- legacy_code/postcard-board/src/App.js
+++ legacy_code/postcard-board/src/App.js
@@ -1,12 +1,10 @@
-import "./App.css";
-import Navbar from "./components/Navbar";
-import Canvas from "./components/Canvas";
+import './App.css';
+import Canvas from './components/Canvas';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Navbar />
         <h1>Hello</h1>
         <Canvas />
       </header>
